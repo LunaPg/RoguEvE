da9bd50c57bae044bbbdf4a4eca188ea43318882
Luna <malalunapg@gmail.com>
2016-02-28 23:04:22 -0500

    Add mock controller to get drones on main view, add mocked drones to team class, display dronesPrefab with API data

---
+++ Assets/Scripts/MainMockDrones.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMockDrones : MonoBehaviour {
+
+	public bool init = false;
+	CrestService crest;
+
+	// Use this for initialization
+	void Start () {
+		crest = GameObject.FindGameObjectWithTag ("CREST").GetComponent<CrestService> ();
+		crest.SendMessage("GetLightScoutDronesT1");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (init == false && crest.gotLightScoutDronesT1 == true ){
+				Debug.Log ("GotlightScou >>" + crest.gotLightScoutDronesT1);
+
+				Team team = GetComponent<Team> ();
+				Debug.Log ("GotlightScou >>" + crest.gotLightScoutDronesT1);
+				
+				team.team = crest.lightScoutDronesT1;
+				team.getDronesGameObjects ();
+				
+				//foreach (Drone dronedata in crest.lightScoutDronesT1) {
+					//GameObject display = Instantiate (GameObject.FindGameObjectWithTag ("droneDetail"));
+					//Text texts = display.GetComponentInChildren<Text>();
+					//foreach (Text text in texts) {
+					//		Debug.Log (text.text);
+					//	}
+
+					//Debug.Log ("GET DA DRONES !" + dronedata.eveId);
+					//crest.gotLightScoutDronesT1 = false;
+			init = true;
+		}
+	}
+}
--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 
 public class Team : MonoBehaviour {
+	
+	//public List<Drone> team = new List<Drone>();
 	public ArrayList team = new ArrayList();
+	public GameObject DronePrefab;
 
-	// Use this for initialization
-	void Start () {
-	
+	public void addDrone(Drone drone){
+		team.Add(drone);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void getDronesGameObjects(){
+		Debug.Log ("DronesGameObject"+team.Count);
+		
+		foreach (Drone drone in team) {
+			GameObject droneClone = (GameObject)Instantiate (DronePrefab, transform.position, transform.rotation);
+			Drone dronefab = droneClone.GetComponent<Drone> () ;
+			dronefab = drone;
+		}
 	}
 }
