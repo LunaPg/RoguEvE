da325553538b08339e3b50eee11f99ba70a3838d
remy chaumard <r.chaumard@gmail.com>
2016-03-01 23:22:55 -0500

    added aura icon and enable/disable command buttons instead of show/hide

---
+++ Assets/CommandButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CommandButton : MonoBehaviour {
+
+	void Start () {
+		gameObject.GetComponent<Button> ().interactable = false;
+	}
+
+	public void enableButtons () {
+		gameObject.GetComponent<Button> ().interactable = true;
+	}
+
+	public void disableButtons () {
+		gameObject.GetComponent<Button> ().interactable = false;
+	}
+}
