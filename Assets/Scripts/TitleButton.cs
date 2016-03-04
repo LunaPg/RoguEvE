2cb32a110c29d8d3e6eac8f6fbbbb43bdfc284be
Luna <malalunapg@gmail.com>
2016-03-03 20:58:17 -0500

    Add function quit and start to button, modifying anchor of Eve char

--- Assets/Scripts/TitleButton.cs
+++ Assets/Scripts/TitleButton.cs
@@ -4,15 +4,6 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleButton : MonoBehaviour {
-
-	void OnGUI(){
-		if (GUILayout.Button ("Start")) {
-			StartGame ();
-		} else if (GUILayout.Button ("Quit")) {
-			QuitGame ();
-		}
-
-	}
 	
 	public void StartGame(){
 		SceneManager.LoadScene ("MotherBase");
